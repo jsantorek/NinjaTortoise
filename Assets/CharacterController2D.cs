a0e157ddb0900607f246619426dd8ec3803ae169
jsantorek <jakub.santorek@gmail.com>
2016-12-23 01:13:57 +0100

    Initial, rigidbody2d based character controller implementation.

---
+++ Assets/CharacterController2D.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController2D : MonoBehaviour {
+    private Rigidbody2D rigidbody;
+    public float v_walk = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+        rigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        input.Normalize();
+        rigidbody.velocity = input * v_walk;
+	}
+}
