47d0b00ca9b25bb93c0ec81670e17059fb53ed8f
jsantorek <jakub.santorek@gmail.com>
2016-12-23 23:39:33 +0100

    Folder cleanup and addition of exposition stat.

---
+++ Assets/Scripts/CharacterStatus.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterStatus : MonoBehaviour
+{
+    private float e_min = 0.0f;
+    private float e_max = 100.0f;
+    public float exposition = 0.0f;
+
+    public bool isExposed()
+    {
+        return (exposition == 100.0f);
+    }
+
+    public void Expose(float e)
+    {
+        exposition += e;
+        exposition = Mathf.Clamp(exposition, e_min, e_max);
+    }
+
+}
