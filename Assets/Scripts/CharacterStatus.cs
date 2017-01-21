a662ca0dd95c5eff34b013be701aa37c03a3f0ce
jsantorek <jakub.santorek@gmail.com>
2017-01-22 00:57:36 +0100

    Objective prefab done, test GUI, more work on enemy AI

--- Assets/Scripts/CharacterStatus.cs
+++ Assets/Scripts/CharacterStatus.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class CharacterStatus : MonoBehaviour
 {
+    public Text GUI_exposition;
+
+
+
     private float e_min = 0.0f;
     private float e_max = 100.0f;
     public float exposition;
+    private int objectives_all;
+    public int objectives_completed = 0;
 
     void Start()
     {
         exposition = e_min;
+        objectives_all = GameObject.FindGameObjectsWithTag("Objective").Length;
+        GUI_exposition.text = objectives_completed.ToString() + " / " + objectives_all.ToString();
     }
 
     public bool isExposed()
@@ -22,6 +31,14 @@ public class CharacterStatus : MonoBehaviour
         exposition += e;
         exposition = Mathf.Clamp(exposition, e_min, e_max);
         Debug.Log(exposition);
+    }
+
+    public void ObjectiveReached()
+    {
+        objectives_completed++;
+        GUI_exposition.text = objectives_completed.ToString() + " / " + objectives_all.ToString();
+        if (objectives_completed >= objectives_all)
+            Debug.Log("END GAME");
     }
 
 }
---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+    public List<Transform> AI_nodes = new List<Transform>();
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
